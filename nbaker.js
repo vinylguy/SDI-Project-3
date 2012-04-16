4c8bd338bc8eeedc2af2427ededceff30ab9a5a3
vinylguy <vinylguy@fullsail.edu>
2012-04-16 13:25:05 -0700

    Coded initial object, and added first save from Flowchart

--- nbaker.js
+++ nbaker.js
@@ -1,1 +1,9 @@
-alert("JavaScript works!");
+var captain = {
+	realName: "Buzz Lightspeed",
+	rank: "Fleet Captain"
+	rebel: false,
+	fleetShips: [ "Centaur 6", "Rominus 2", "Glactic 7" ],
+	warpTo: function (destination) {},
+	getPosition: function () {}
+};
+
